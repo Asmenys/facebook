a02a55c2f2ad5e0bdd8cabae8b8b1d2bf146704a
Asmenys <76791232+Asmenys@users.noreply.github.com>
2022-07-27 10:38:23 +0000

    changed user column names to snakecase

--- db/migrate/20220726115811_add_name_to_user.rb
+++ db/migrate/20220726115811_add_name_to_user.rb
@@ -1,6 +1,6 @@
 class AddNameToUser < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :firstName, :string
-    add_column :users, :lastName, :string
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
   end
 end
