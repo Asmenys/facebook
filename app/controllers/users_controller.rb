266fb9933bf93101a3e8c70c3a21a096c1c42b6c
Asmenys <76791232+Asmenys@users.noreply.github.com>
2022-07-31 14:13:41 +0300

    reset the root path to the user session creation page, if the user is signed in then it redirects to his home page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,5 @@
 class UsersController < ApplicationController
     def show
-        @user = nil
-        if params.key?(:id)
-            @user = User.find(params[:id])
-        elsif user_signed_in?
-            @user = current_user
-        else
-            redirect_to new_user_session_url
-        end
+        @user = User.find(params[:id])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,9 @@ Rails.application.routes.draw do
   resources :posts
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
   devise_scope :user do
-    root to: "users#show"
+    root "devise/sessions#new"
   end
 end
