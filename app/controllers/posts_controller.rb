74031122fa2d4c4fc63f1b0644f4b0f092118e54
Asmenys <76791232+Asmenys@users.noreply.github.com>
2022-08-03 16:59:46 +0300

    moved posts in users/show.html.erb to a turbo frame, enclosed post partial in a turbo frame tag, fixed delete functionality, now it updates html after deletion of a post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,6 +21,16 @@ class PostsController < ApplicationController
        @post = Post.find(params[:id]) 
     end
 
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+        
+        respond_to do |format|
+            format.turbo_stream { render turbo_stream: turbo_stream.remove(@post) }
+            format.html { redirect_to posts_url }
+        end
+    end
+
     private
 
     def post_params
