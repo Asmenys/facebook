0bccda00a33f5ef8174ed6be4857ca3321b1efd7
Asmenys <76791232+Asmenys@users.noreply.github.com>
2022-08-08 17:26:46 +0300

    changed nested routing for users/posts to use a separate controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :posts
   resources :users do
-    resources :posts
+    resources :posts, controller: "user_posts"
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
