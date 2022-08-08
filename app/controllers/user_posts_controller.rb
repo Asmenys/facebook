5b52ece088fc64ec2701b32bae71e482c796e4e6
Asmenys <76791232+Asmenys@users.noreply.github.com>
2022-08-08 17:29:32 +0300

    UserPostsController now inherits from PostsController

--- app/controllers/user_posts_controller.rb
+++ app/controllers/user_posts_controller.rb
@@ -1,2 +1,5 @@
-class UserPostsController < ApplicationController
+class UserPostsController < PostsController
+    def index
+        @posts = Post.all.where(creator_id: params[:user_id])
+    end
 end
