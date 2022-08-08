20a77cb8a8ddfa5c569c7d32aafad27b79346097
Asmenys <76791232+Asmenys@users.noreply.github.com>
2022-08-08 12:20:53 +0300

    posts button now displays user posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :posts
-  resources :users
+  resources :users do
+    resources :posts
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
