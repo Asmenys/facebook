1e0954415720d76107113bf6b3dd61726b7e4d07
Asmenys <76791232+Asmenys@users.noreply.github.com>
2022-08-14 09:26:40 +0300

    the users show page now always displays user posts on load, replacing the page information works flawlessly now

--- app/javascript/controllers/user_page_controller.js
+++ app/javascript/controllers/user_page_controller.js
@@ -1,10 +1,14 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-    static targets = ["userPageBody"]
-    updateUserPageBody(event){
+    
+    static targets = ["userPageBody", "userPageBodyContent"]
+
+    replaceUserPageBody(event){
         let new_frame = document.createElement("turbo-frame")
         new_frame.id = event.params.frameName
-        this.userPageBodyTarget.replaceChild(new_frame, this.userPageBodyTarget.firstChild)
+        new_frame.setAttribute("data-user-page-target", "userPageBodyContent")
+        this.userPageBodyTarget.replaceChild(new_frame, this.userPageBodyContentTarget)
     }
+
 }
