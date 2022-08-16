1862c87a598300ee0a09607633fbaab37921c52c
Asmenys <76791232+Asmenys@users.noreply.github.com>
2022-08-16 14:52:17 +0300

    reformated user_page_controller, added functionality to change the style of selected div, added similar functionality to the divs themselves

--- app/javascript/controllers/user_page_controller.js
+++ app/javascript/controllers/user_page_controller.js
@@ -9,16 +9,34 @@ export default class extends Controller {
         this.currentDisplayPageValue = "posts"
     }
     replaceUserPageBody(event){
-        this.getTarget(this.currentDisplayPageValue).hidden= true
-        this.currentDisplayPageValue = event.params.frameName
-        this.getTarget(this.currentDisplayPageValue).hidden= false
+        this.hideCurrentTarget()
+        this.unhideNewTarget(event.params.frameName)
     }
+    hideCurrentTarget(){
+        this.getTarget(this.currentDisplayPageValue).hidden = true
+        this.resetStyle(this.currentDisplayPageValue)
+    }
+    unhideNewTarget(frameName){
+        this.setSelectedStyle(frameName)
+        this.currentDisplayPageValue = frameName
+        this.getTarget(this.currentDisplayPageValue).hidden = false
+    }
+
     getTarget(targetName){
         return targetName == "posts" ? this.postsTarget
                 : targetName == "about" ? this.aboutTarget
                 : targetName == "friends" ? this.friendsTarget
                 : targetName == "friend_requests" ? this.friend_requestsTarget
                 : null
+    }
+
+    resetStyle(targetId){
+        document.getElementById(targetId).className = "p-4 pb-2 select-none cursor-pointer"
+    }
+
+    setSelectedStyle(targetId){
+        let target = document.getElementById(targetId)
+        target.className = "p-4 pb-2 select-none cursor-pointer border-b-2 border-solid border-blue-500 text-blue-500"
     }
 
 }
