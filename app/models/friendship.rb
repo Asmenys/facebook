ba00c434da83d02026ff2a8401a70683a46d9c1b
Asmenys <76791232+Asmenys@users.noreply.github.com>
2022-08-19 15:53:59 +0300

    friendship model update

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -2,5 +2,12 @@ class Friendship < ApplicationRecord
     belongs_to :friendship_master, class_name: "User"
     belongs_to :friendship_receiver, class_name: "User"
 
+    def friend(user_id)
+        if user_id != self.friendship_master_id
+            self.friendship_master
+        else
+            self.friendship_receiver
+        end
+    end
     scope :friendships_for_user, ->(user_id) {self.where(:friendship_receiver_id == user_id).or(self.where(:friendship_master_id == user_id))}
 end
